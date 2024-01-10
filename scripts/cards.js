e5db95794ddc99299ab941d3ef80aef301de0067
Andrei.Maiorov <rew081292@yandex.ru>
2024-01-10 20:36:23 +0700

    Final version

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      description: "Архыз. Кавказский хребет"
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      description: "Челябинская область. Река в лесу зимой"
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      description: "Иваново. Многоэтажки города"
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      description: "Камчатка. Подошва горы"
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      description: "Холмогорский район. Железнодорожный путь в лесу"
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      description: "Байкал. Обрыв у озера зимой"
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsContainer = document.querySelector('.places__list');
+
+
+initialCards.forEach(function (item) {
+  const card = createCard(item);
+  cardsContainer.append(card);
+});
+
+function createCard(element) {
+
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.description;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  cardDeleteButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
